4bf2e1c353a6c7a80d5e33a7fa25aae760840255
Devin Markell <dmarkell@gmail.com>
2016-03-30 01:34:30 -0400

    hooked up psql

--- sabrfilter.py
+++ sabrfilter.py
@@ -1,12 +1,15 @@
+
 import datetime
 import json
-import requests
 import psycopg2
+import requests
+
 from psycopg2.extras import RealDictCursor
 from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash, send_from_directory
 from flask.ext.heroku import Heroku
 from contextlib import closing
+import urlparse
 import os
 
 TOKEN = os.environ['TOKEN']
@@ -16,6 +19,7 @@ is_prod = os.environ.get('IS_HEROKU', None)
 if is_prod:
     app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
     heroku = Heroku(app)
+    db_con = psycopg2.connect()
 
 else:
     app.config['DEBUG'] = True
@@ -23,8 +27,18 @@ else:
         os.environ['PG_DATABASE_PATH'],
         os.environ['PG_DATABASE_NAME']
     )
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-db_con = psycopg2.connect()
+    db_con = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+
+
 
 @app.route('/')
 def render_page():
