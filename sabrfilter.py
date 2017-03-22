30e67a2d77b961d353a11d4190cda07599c52923
Devin Markell <dmarkell@gmail.com>
2017-03-22 16:58:53 -0400

    adding bs

--- sabrfilter.py
+++ sabrfilter.py
@@ -1,4 +1,5 @@
 
+from bs4 import BeautifulSoup
 import datetime
 import json
 import psycopg2
@@ -38,6 +39,85 @@ else:
 def render_page():
     return app.send_static_file('index.html')
 
+
+@app.route('/espn_fantasy/get_closers')
+def get_closers():
+
+    return json.dumps([el for el in _get_closers()])
+
+def _get_closers():
+
+    rp_types = set(["CLOSER", "NEXT_UP", "SETUP", "SLEEPER", "FUTURE", "LOOMING", "BATTLE", "COMMITTEE", "CO-CLOSERS"])
+    errata = {
+        28614: 30213,
+        5064: 33089
+    }
+
+    repl_many = lambda w, z: reduce(lambda x, y: x.replace(y, ''), [w] + z)
+    _clean = lambda x: x.replace(":", "").replace(" ", "_")
+    url = 'http://sports.espn.go.com/fantasy/baseball/flb/story?page=REcloserorgchart'
+    resp = requests.get(url).content
+    soup = BeautifulSoup(resp)
+
+    tbody = soup.find('table', attrs={'class': 'inline-table'}).find('tbody')
+
+    rows = tbody.findAll('tr')
+
+    # go through row groups
+    row_group_size = 2
+    for rx in xrange(len(rows)/row_group_size):
+        team_codes = []
+        for td in rows[rx*row_group_size].findAll('td'):
+            if td.img:
+                code = td.img['src'].split('/')[-1].split('.')[0]
+                team_codes.append(code)
+
+        row_group_contents = rows[rx*row_group_size+1].findAll('td')
+        # go through columns
+        for ix, team_code in enumerate(team_codes):
+
+            team_contents = [el for el in row_group_contents[ix+1].children]
+
+            rp_type = ''
+            rps = []
+            for child in team_contents:
+                try:
+                    clean_text = _clean(child.get_text())
+                    if clean_text in rp_types:
+                        if rps:
+                            for rp in rps:
+                                print(json.dumps({
+                                    "team_code": team_code,
+                                    "role": rp_type,
+                                    "player_id": rp[0] if rp[0] not in errata else errata[rp[0]],
+                                    "player_name": rp[1]
+                                }))
+                        rp_type = clean_text
+                        rps = []
+                    else:
+                        if clean_text != '':
+                            try:
+                                player_id = int(child['href'].split('/')[-2])
+                            # adapt to new category types:
+                            except KeyError:
+                                rp_types.add(clean_text)
+                            player_name = child.get_text()
+                            rps.append((player_id, player_name))
+
+                except AttributeError:
+                    continue
+
+            # clear out whatever is left:
+            if rps:
+                for rp in rps:
+                    yield {
+                        "team_code": team_code,
+                        "role": rp_type,
+                        "player_id": rp[0] if rp[0] not in errata else errata[rp[0]],
+                        "player_name": rp[1]
+                    }
+
+
 @app.route('/data_batting')
 def query_db_batting():
 
@@ -64,7 +144,7 @@ def query_db_pitching():
 def get_config():
 
     config = {
-        "version": 'v0.1.0'
+        "version": 'v0.1.1'
     }
 
     return json.dumps(config)
